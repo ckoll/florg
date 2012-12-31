13f2dcd7498823d7ac3fa5bdca8b23bafe89dca9
ck <tkitsch08@gmail.com>
2012-12-31 04:25:39 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fanlink::Application.config.secret_token = '2edcad3f50720b85a934de1686cd082dd68c58463a3f678f09f8750b6dfd1a50a6027ccd10db9e1093b8f5d9bd920d69d161d62a7e33b6407c9a5a4f1256d58d'
